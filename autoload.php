6497a456559e087f81287b175c37181976a7271d
Jowi <joaobalaniuc>
2023-03-02 15:15:07 -0300

    start

---
+++ autoload.php
@@ -0,0 +1,8 @@
+<?php
+// Includes
+require_once(__DIR__ . "/src/MyService.php");
+require_once(__DIR__ . "/src/MySchema.php");
+require_once(__DIR__ . "/helpers/jwquery.php");
+
+// Aliases
+class_alias("MyService", "my");
