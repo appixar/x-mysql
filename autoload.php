766f78e53980cc925bd3340e177f4d5024fcec39
Jowi <joaobalaniuc>
2023-03-02 15:07:28 -0300

    upd ignore

--- autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Includes
-require_once(__DIR__ . "/src/MyService.php");
-require_once(__DIR__ . "/src/MySchema.php");
-require_once(__DIR__ . "/helpers/jwquery.php");
-
-// Aliases
-class_alias("MyService", "my");
