cc5a7242353e12fd7132c9de00ac0b8d4e5e2a9c
Jowi <joaobalaniuc>
2023-09-06 14:24:19 -0300

    upd ignore

--- autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Includes
-require_once(__DIR__ . "/src/MyService.php");
-require_once(__DIR__ . "/src/MySchema.php");
-require_once(__DIR__ . "/src/MyValidate.php");
-require_once(__DIR__ . "/helpers/jwquery.php");
-
-// Aliases
-class_alias("MyService", "my");
